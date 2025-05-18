2c363f9f132a348cbc0bb2770a9711bb1b9d3480
YEJI JUNG <yejiii1997@gmail.com>
2025-05-18 13:24:33 +0900

    fix: 소개글 수정

--- blog.config.ts
+++ blog.config.ts
@@ -2,7 +2,7 @@ const blogConfig = {
   title: `YEJILOG`,
   description: `This is a blog about knowledge sharing and study records of front-end developers.`,
   author: `YEJI`,
-  introduction: `내가 모르는게 너무 많아서 작성하기 시작한 블로그 \n 나중의 나를 위한 소중한 자산이 되어주길...`,
+  introduction: `내가 모르는게 너무 많아서 작성하기 시작한 블로그 \n 미래의 나를 위한 소중한 자산이 되어주길...`,
 
   emoji: {
     category: `☘️`,
